25d8f01bf8674dc09e598bdea6754c20d61e057e
H. Shavit <s3092593@uridium.liacs.nl>
2022-05-31 00:09:17 +0200

    Fix task 2

--- task2/task2.py
+++ task2/task2.py
@@ -11,10 +11,11 @@ x0s = []
 x1s = []
 f1s = []
 f2s = []
+
+plot_all_trajectories = False
 for i in range(1001):
     w1 = i / 1000.0
     w2 = 1 - i / 1000.0
-
 
     def objective(x):
         f1 = 0.5 * np.power(x[0], 2) * x[1]
@@ -24,7 +25,6 @@ for i in range(1001):
         return np.maximum(w1 * np.abs(f1 - r1), w2 * np.abs(f2 - r2))
 
     # black-box optimization software
-
     def local_hillclimber(objective, bounds, n_iterations, step_size, init):
         # generate an initial point
         best = init
@@ -34,13 +34,18 @@ for i in range(1001):
         scores = list()
         points = list()
         for i in range(n_iterations):  # take a step
-            candidate = [curr[0] + rand() * step_size[0] - step_size[0] / 2.0,
-                         curr[1] + rand() * step_size[1] - step_size[1] / 2.0]
+            
+            # candidate = [curr[0] + rand() * step_size[0] - step_size[0] / 2.0,
+                        #  curr[1] + rand() * step_size[1] - step_size[1] / 2.0]
+            candidate = [np.random.uniform(max(bounds[0, 0], curr[0] - step_size[0]), min(bounds[0, 1], curr[0] + step_size[0])),
+                        random.uniform(max(bounds[1, 0], curr[1] - step_size[1]), min(bounds[1, 1], curr[1] + step_size[1])) ]
             # print(candidate)
             if candidate[0] > bounds[0, 1]:
                 candidate[0] = curr[0]
+                print('gg')
             if candidate[1] > bounds[1, 1]:
                 candidate[1] = curr[1]
+                print('dsa')
             points.append(candidate)
             # print('>%d f(%s) = %.5f, %s' % (i, best, best_eval, candidate))
             # evaluate candidate point
@@ -55,8 +60,8 @@ for i in range(1001):
         return [best, best_eval, points, scores]
 
     bounds = asarray([[0, 5], [0, 10]])
-    step_size = [0.2, 0.2]
-    n_iterations = 1000
+    step_size = [0.4, 0.4]
+    n_iterations = 10000
     init = [2.4, 2.0]
 
     best, score, points, scores, = local_hillclimber(objective, bounds, n_iterations, step_size, init)
@@ -66,32 +71,33 @@ for i in range(1001):
     f1s.append(0.5 * np.power(best[0], 2) * best[1])
     f2s.append(np.power(best[0], 2) + 3 * best[0] * best[1])
 
-    n, m = 7, 7
-    start = -3
-    x_vals = np.arange(0, 5, 0.1)
-    y_vals = np.arange(0, 10, 0.1)
-    X, Y = np.meshgrid(x_vals, y_vals)
+    if plot_all_trajectories:
+        n, m = 7, 7
+        start = -3
+        x_vals = np.arange(0, 5, 0.1)
+        y_vals = np.arange(0, 10, 0.1)
+        X, Y = np.meshgrid(x_vals, y_vals)
 
-    # print(X.shape)
-    # print(Y.shape)
+        # print(X.shape)
+        # print(Y.shape)
 
-    fig = plt.figure(figsize=(6, 5))
-    left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
-    ax = fig.add_axes([left, bottom, width, height])
-    # Z = 0.1 * np.abs((0.5 * np.power(X, 2) * Y)) +
-    # 0.9 * np.abs((np.power(X, 2) + 3 * X * Y) - 250)
-    Z = objective([X, Y])
-    # print(Z)
+        fig = plt.figure(figsize=(6, 5))
+        left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
+        ax = fig.add_axes([left, bottom, width, height])
+        # Z = 0.1 * np.abs((0.5 * np.power(X, 2) * Y)) +
+        # 0.9 * np.abs((np.power(X, 2) + 3 * X * Y) - 250)
+        Z = objective([X, Y])
+        # print(Z)
 
-    # cp = ax.contour(X, Y, Z)
-    # ax.clabel(cp, inline=True, fontsize=10)
-    # ax.set_title(f'Contour Plot for w1={round(w1,2)}, w2={round(w2,2)}', fontsize=20)
-    # ax.set_xlabel('x[0]')
-    # ax.set_ylabel('x[1]')
+        cp = ax.contour(X, Y, Z)
+        ax.clabel(cp, inline=True, fontsize=10)
+        ax.set_title(f'Contour Plot for w1={round(w1,2)}, w2={round(w2,2)}', fontsize=20)
+        ax.set_xlabel('x[0]')
+        ax.set_ylabel('x[1]')
 
-    # for i in range(n_iterations):
-    #     plt.plot(points[i][0], points[i][1], "o")
-    # plt.savefig(f'pareto{round(w1,2)}_{round(w2,2)}.png')
+        for i in range(n_iterations):
+            plt.plot(points[i][0], points[i][1], "o")
+        plt.savefig(f'pareto{round(w1,2)}_{round(w2,2)}.png')
 
 plt.clf()
 random_x1s = random.rand(1000, 1) * (5.0)
